560640dce58f7db9438d80f1eb08dc8fc2caa6dc
Ganesh <chinthakindiganesh94@gmail.com>
2025-07-26 00:51:41 +0530

    check-in

---
+++ force-app/main/default/lwc/lwcOUT/lwcOUT.js
@@ -0,0 +1,8 @@
+
+Aura application (LA_LWC_OUT)
+============================
+
+<aura:application extends="ltng:outApp" >   
+    <aura:dependency resource="c:imparativeWithTable"/>
+    <aura:dependency resource="c:calculatorComponenet"/>
+</aura:application>
